b64d126fe23f4c53cf4f7e46c3ed7e3399ce5c39
njohnerwin <njohnerwin@gmail.com>
2020-06-30 19:55:26 -0400

    Added gitignore

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ app.use(express.json());
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 })
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
