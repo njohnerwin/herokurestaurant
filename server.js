2b31b09ea4eb041abd87404bf796ce0346ae728c
njohnerwin <njohnerwin@gmail.com>
2020-06-30 21:21:52 -0400

    Fixed so API post works

--- server.js
+++ server.js
@@ -12,10 +12,37 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+var reservations = [
+    {
+        name: "Nicholas",
+        phonenumber: "413-555-9137",
+        email: "hodouvan.66@gmail.com",
+        uniqueid: "nick",
+    }
+]
+
+
+
 app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 })
 
+app.get("/add", function(req, res) {
+    res.sendFile(path.join(__dirname, "add.html"));
+});
+
+app.post("/api/reservations", function(req, res) {
+    
+    var newReservation = req.body;
+  
+    console.log(newReservation);
+  
+    reservations.push(newReservation);
+  
+    res.json(newReservation);
+});
+
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
 });
+
